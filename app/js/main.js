36878a04d53cb0448c016a256af425af64e32563
Misha-web <60877381+Misha-web@users.noreply.github.com>
2020-06-08 16:55:55 +0300

    blocks header and slider is made

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
-	
+	// Burger menu
+	$('.burger-menu').on("click", function() {
+		$('.burger-menu, .nav ').toggleClass('active');
+	});
+
+	// Slider
+	$('.slider').slick({
+		dots: true,
+		slidesToShow: 4,
+		slidesToScroll: 2,
+		infinite: true,
+		responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				arrows: false,
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				arrows: false,
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
+
+	// Fixed header
+	let headerH = $("#header__actions").innerHeight();
+	let headerMenu = $("#header__menu");
+	let scrollOffset = 0;
+	$(window).on("scroll", function() {
+		scrollOffset = $(this).scrollTop();
+		if(scrollOffset >= headerH) {
+			headerMenu.addClass("fixed");
+		} else {
+			headerMenu.removeClass("fixed");
+		}
+	});
 });
 
 
